6d9e050deb53d6a809591a89133e0588aee3a4a6
Alex Tai <alextai@Alexs-Air.lan>
2022-11-10 21:42:18 -0800

    Get all Reviews of Current User

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,74 +1,71 @@
 // backend/routes/api/users.js
-const express = require('express')
+const express = require("express");
 
-const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User, Spot } = require('../../db/models');
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const { User, Spot, Review, ReviewImage } = require("../../db/models");
 
-const { check } = require('express-validator');
-const { handleValidationErrors } = require('../../utils/validation');
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+const review = require("../../db/models/review");
 
 // const { requireAuth } = require('/../../utils/auth');
 
 const router = express.Router();
 
 const validateSignup = [
-  check('email')
+  check("email")
     .exists({ checkFalsy: true })
     .isEmail()
-    .withMessage('Please provide a valid email.'),
-  check('username')
+    .withMessage("Please provide a valid email."),
+  check("username")
     .exists({ checkFalsy: true })
     .isLength({ min: 4 })
-    .withMessage('Please provide a username with at least 4 characters.'),
-  check('username')
-    .not()
-    .isEmail()
-    .withMessage('Username cannot be an email.'),
-  check('password')
+    .withMessage("Please provide a username with at least 4 characters."),
+  check("username").not().isEmail().withMessage("Username cannot be an email."),
+  check("password")
     .exists({ checkFalsy: true })
     .isLength({ min: 6 })
-    .withMessage('Password must be 6 characters or more.'),
-  handleValidationErrors
+    .withMessage("Password must be 6 characters or more."),
+  handleValidationErrors,
 ];
 
 // Sign up
-router.post(
-  '/',
-  validateSignup,
-  async (req, res, next) => {
-    const { email, password, username, firstName, lastName } = req.body;
+router.post("/", validateSignup, async (req, res, next) => {
+  const { email, password, username, firstName, lastName } = req.body;
 
-    const existingUser = await User.findOne({
-      where: {
-        email
-      }
-    })
+  const existingUser = await User.findOne({
+    where: {
+      email,
+    },
+  });
 
-    if (existingUser) {
-      const err = new Error("User with that email already exists");
-      err.status = 403;
-      return next(err)
-    };
-    
-    const user = await User.signup({ email, username, password, firstName, lastName });
-    user.dataValues.token = await setTokenCookie(res, user);
-    
-    return res.json(
-      user,
-    );
+  if (existingUser) {
+    const err = new Error("User with that email already exists");
+    err.status = 403;
+    return next(err);
   }
 
-);
+  const user = await User.signup({
+    email,
+    username,
+    password,
+    firstName,
+    lastName,
+  });
+  user.dataValues.token = await setTokenCookie(res, user);
 
-router.get('/me/spots', requireAuth, async (req, res) => {
+  return res.json(user);
+});
+
+router.get("/me/spots", requireAuth, async (req, res) => {
   const spotsOfUser = await Spot.findAll({
     where: {
-      ownerId: req.user.id 
-    }
-  })
+      ownerId: req.user.id,
+    },
+  });
 
   return res.json({
-    Spots: spotsOfUser
+    Spots: spotsOfUser,
   });
 });
 
@@ -79,5 +76,57 @@ router.get('/me/spots', requireAuth, async (req, res) => {
 //     user.dataValues.token = await setTokenCookie(res, user)
 //   )
 // });
+
+// GET ALL REVIEWS OF CURRENT USER
+router.get("/me/reviews", requireAuth, async (req, res, next) => {
+  const currentUser = req.user.id;
+  const Reviews = await Review.findAll({
+    where: {
+      userId: currentUser,
+    },
+    attributes: {
+      include: [
+        'id',
+        "userId",
+        "spotId",
+        "review",
+        "stars",
+        "createdAt",
+        "updatedAt",
+      ],
+    },
+    include: [
+      {
+        model: User,
+        attributes: [
+          'id',
+          "firstName", 
+          "lastName"
+        ],
+      },
+      {
+        model: Spot,
+        attributes: [
+          'id',
+          "ownerId",
+          "address",
+          "city",
+          "state",
+          "country",
+          "lat",
+          "lng",
+          "name",
+          "price",
+          "previewImage",
+        ],
+      },
+      {
+        model: ReviewImage,
+        attributes: [ 'id','url' ]
+    },
+    ],
+  });
+  return res.json({Reviews});
+});
 
 module.exports = router;
