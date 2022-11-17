9becdb4f7447c28a7f91e97a9f8fab1af19fdfbe
Alex Tai <alextai@Alexs-Air.lan>
2022-11-16 19:56:29 -0800

    Moved validators to validation.js

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -17,105 +17,12 @@ const {
 const { Sequelize, fn, Op } = require("sequelize");
 
 const { check } = require("express-validator");
-const { handleValidationErrors } = require("../../utils/validation");
+const { handleValidationErrors, validateSpot, validateReview, validateQueryParameters } = require("../../utils/validation");
 const review = require("../../db/models/review");
 const user = require("../../db/models/user");
 const { route } = require("./users");
 
 const router = express.Router();
-
-const validateSpot = [
-  check("address")
-    .exists({ checkFalsy: true })
-    .withMessage("Street address is required"),
-  check("city").exists({ checkFalsy: true }).withMessage("City is required"),
-  check("state").exists({ checkFalsy: true }).withMessage("State is required"),
-  check("country")
-    .exists({ checkFalsy: true })
-    .withMessage("Country is required"),
-  check("lat")
-    .exists({ checkFalsy: true })
-    .withMessage("Latitude is not valid"),
-  check("lng")
-    .exists({ checkFalsy: true })
-    .withMessage("Longitude is not valid"),
-  check("name")
-    .exists({ checkFalsy: true })
-    .isLength({ max: 50 })
-    .withMessage("Name must be less than 50 characters"),
-  check("description")
-    .exists({ checkFalsy: true })
-    .withMessage("Description is required"),
-  check("price")
-    .exists({ checkFalsy: true })
-    .withMessage("Price per day is required"),
-  handleValidationErrors,
-];
-
-const validateReview = [
-  check("review")
-    .exists({ checkFalsy: true })
-    .withMessage("Review text is required"),
-  check("stars")
-    .exists({ checkFalsy: true })
-    .isInt({ min: 1, max: 5 })
-    .withMessage("Stars must be an integer from 1 to 5"),
-  handleValidationErrors,
-];
-
-const validateQueryParameters = [
-  check("page")
-    .optional()
-    .exists({ checkFalsy: true })
-    .optional()
-    .isInt({ min: 0, max: 10 })
-    .withMessage("Page must be greater than or equal to 0"),
-  check("size")
-    .optional()
-    .exists({ checkFalsy: true })
-    .optional()
-    .isInt({ min: 0, max: 20 })
-    .withMessage("Size must be greater than or equal to 0"),
-  check("minLat")
-    .optional()
-    .exists({ checkFalsy: true })
-    .optional()
-    .isDecimal()
-    .withMessage("Minimum latitude is invalid"),
-  check("maxLat")
-    .optional()
-    .exists({ checkFalsy: true })
-    .optional()
-    .isDecimal()
-    .withMessage("Maximum latitude is invalid"),
-  check("minLng")
-    .optional()
-    .exists({ checkFalsy: true })
-    .optional()
-    .isDecimal()
-    .withMessage("Minimum longitude is invalid"),
-  check("maxLng")
-    .optional()
-    .exists({ checkFalsy: true })
-    .optional()
-    .isDecimal()
-    .withMessage("Maximum longitude is invalid"),
-  check("minPrice")
-    .optional()
-    .exists({ checkFalsy: true })
-    .optional()
-    .isDecimal()
-    .isInt({ min: 0 })
-    .withMessage("Minimum price must be greater than or equal to 0"),
-  check("maxPrice")
-    .optional()
-    .exists({ checkFalsy: true })
-    .optional()
-    .isDecimal()
-    .isInt({ min: 0 })
-    .withMessage("Maximum price must be greater than or equal to 0"),
-  handleValidationErrors,
-];
 
 // router.get("/", async (req, res) => {
 //   const allSpots = await Spot.findAll();
