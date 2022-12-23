d2385778a764372ae2697d256b8aa16048604b8a
alexandratai <alextai@Alexs-Air.lan>
2022-12-22 21:23:47 -0800

    Removed extra code

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,6 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
-const apiRouter = require('./api');
-
-router.use('/api', apiRouter);
 
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
